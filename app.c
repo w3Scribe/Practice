43d13d76bc6613c17a5bce983b05b64320518e50
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-07-11 15:29:11 +0530

    chore: Refactor code to calculate average and sum of student marks

--- app.c
+++ app.c
@@ -6,15 +6,26 @@
 
 #include<stdio.h>
 
+#define NUMBER_OF_STD 5
+
 int main(){
-  int number;
-  printf("Enter a number: ");
-  scanf("%d", &number);
-  printf("You entered: %d\n", number);
-  long long int factorial = 1;
-  for(int i=1; i<=number; i++) {
-    factorial = factorial * i;
+  int std[NUMBER_OF_STD], marks[NUMBER_OF_STD];
+  float total_marks = 0, avg_marks;
+
+  printf("\nEnter the roll number of %d std : \n", NUMBER_OF_STD);
+
+  for(int i = 0; i<NUMBER_OF_STD; i++)
+    scanf("\n%d", &std[i]);
+
+  for(int j = 0; j<NUMBER_OF_STD; j++){
+    printf("\nEnter the marks of std %d : ", std[j]);
+    scanf("%d", &marks[j]);
+    total_marks += marks[j];
   }
-  printf("Factorial of %d is %lld\n", number, factorial);
+
+  avg_marks = total_marks/NUMBER_OF_STD;
+  printf("\nTotal marks is : %.2f", total_marks);
+  printf("\nAverage mark of std is : %.2f", avg_marks);
+
   return 0;
 }
