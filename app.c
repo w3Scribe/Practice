a3815b91203fc4f341535ce933b4fc1da9eca9a7
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-07-16 21:23:44 +0530

    Refactor code to add element at a specific position in an array

--- app.c
+++ app.c
@@ -1,10 +1,39 @@
-
-// Arithemetic conversion
+// Traversal, Insertion, Deletion, Searching, Sorthing
 #include<stdio.h>
 
 void main(){
-  int a = 10;
-  int b = 'b'; //
-  int sum = a + b;
-  printf("Sum: %d\n", sum);
+  int arr[50];
+  int arrSize;
+  int pos, number;
+  
+  printf("Enter the length : ");
+  scanf("%d", &arrSize);
+
+  printf("Enter the array elements :\n");
+  for(int i=0; i<arrSize; i++){
+    scanf("%d",&arr[i]);
+  }
+
+  printf("\nYou have entered the following array : \n");
+  for(int i=0; i<arrSize; i++){
+    printf("%d ",arr[i]);
+  }
+
+  printf("\nEnter the position of element that you want to insert : ");
+  scanf("%d", &pos);
+
+  printf("Enter the element that you want to insert : ");
+  scanf("%d", &number);
+
+  for(int i=arrSize; i>=pos; i--){
+    arr[i] = arr[i-1];
+  }
+
+  arr[pos] = number;
+  arrSize +=1;
+
+  for(int i=0; i<arrSize; i++){
+    printf("%d ",arr[i]);
+  }
+
 }
