9b837f461990fd6104c8a81de36f7b1ccbd75b57
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-07-25 20:07:33 +0530

    Refactor code to input two numbers from the keyboard and print their sum

--- app.c
+++ app.c
@@ -1,42 +1,14 @@
-// Traversal, Insertion, Deletion, Searching, Sorthing
-// strlen, strrev, strcpy, strcat, strupr, strlwr
+// WAP to input two numbers from the keyboard and print their sum.
+#include<stdio.h>
 
-#include <stdio.h>
-// 1 2 3 4 5
 int main(){
- int arrSize = 5;
- int arr[arrSize];
- int pos, element;
+ int a, b;
+ printf("Enter the value of a : ");
+ scanf("%d", &a);
 
- printf("Enter the arry element : ");
- for(int i=0; i<arrSize; i++){
-   scanf("%d", &arr[i]);
- }
- 
- printf("you have entered the following array elements : \n");
- for(int i=0; i<arrSize; i++){
-   printf("%d\t", arr[i]);
- }
- 
- printf("\nEnter the postion of element : ");
- scanf("%d", &pos);
- printf("Enter the element : ");
- scanf("%d", &element);
+ printf("Enter the value of b : ");
+ scanf("%d", &b);
 
- if(pos < 0 || pos == arrSize){
-   printf("Invalid position!\n");
-   return 1;
- }
-
- for(int i=arrSize; i>=pos-1; i--){
-   arr[i] = arr[i-1];
- }
- 
- arr[pos-1] = element;
- arrSize++;
-
- for(int i=0; i<arrSize; i++){
-    printf("%d\t", arr[i]);
- }
-
+ printf("The sum of a+b = %d", a+b);
+ return 0;
 }
