330cc958cbbb55b6c8a1ed1952076149794c5097
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-08-02 11:53:09 +0530

    Refactor code to add SWAP functionality for three numbers

--- app.c
+++ app.c
@@ -1,24 +1,28 @@
-// WAP to SWAP (interchange) 2 numbers without using third variable.
+// WAP to SWAP (interchange) three numbers
+
 #include<stdio.h>
 
 int main(){
-  int a, b;
-
-  printf("Enter the value of A : ");
+  int a, b, c, d;
+  
+  printf("Enter the value of A :  ");
   if(!scanf("%d", &a)) goto INVALID_INPUT;
 
-  printf("Enter the value of B : ");
+  printf("Enter the value of B :  ");
   if(!scanf("%d", &b)) goto INVALID_INPUT;
- 
-  a = a + b;  
-  b = a - b;
-  a = a - b;
 
-  printf("The value of A and B is %d %d", a, b);
-  
+  printf("Enter the value of C :  ");
+  if(!scanf("%d", &c)) goto INVALID_INPUT;
+
+  d = a + b + c;
+  b = d - c;
+  a = c - a;
+  c = b - a;
+
+  printf("The value of \nA : %d\nB : %d\nC : %d", a, b, c);
+
   return 0;
-
   INVALID_INPUT : 
-    printf("Invalid Input.");
-    return 0;
+     printf("Invalid Input.");
+     return 0;
 }
