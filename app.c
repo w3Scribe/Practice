69f84f6f3eaf9913d91a8abd371a096655050d75
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-08-03 09:58:25 +0530

    Refactor code to add SWAP functionality for four numbers

--- app.c
+++ app.c
@@ -1,9 +1,9 @@
-// WAP to SWAP (interchange) three numbers
+// WAP to SWAP four numbers without using five variable
 
 #include<stdio.h>
 
 int main(){
-  int a, b, c, temp;
+  int a, b, c, d;
   
   printf("Enter the value of A :  ");
   if(!scanf("%d", &a)) goto INVALID_INPUT;
@@ -14,12 +14,22 @@ int main(){
   printf("Enter the value of C :  ");
   if(!scanf("%d", &c)) goto INVALID_INPUT;
 
-  temp = a;
-  a = b;
-  b = c;
-  c = temp;
+  printf("Enter the value of D :  ");
+  if(!scanf("%d", &d)) goto INVALID_INPUT;
 
-  printf("The value of \nA : %d\nB : %d\nC : %d", a, b, c);
+  // a = a + b + c + d;
+  // d = a - (b + c + d);
+  // c = a - (b + c + d);
+  // b = a - (b + c + d);
+  // a = a - (b + c + d);
+
+  a = a + b + c + d;
+  b = a - (b + c + d);
+  c = a - (b + c + d);
+  d = a - (b + c + d);
+  a = a - (b + c + d);
+
+  printf("The value of \nA : %d\nB : %d\nC : %d\nD : %d", a, b, c, d);
 
   return 0;
   INVALID_INPUT : 
