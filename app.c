1320e78e99e9727e56f878003ad98e0e4c1c9d49
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-08-03 11:00:22 +0530

    code to print the reverse of a 3 digit number

--- app.c
+++ app.c
@@ -1,26 +1,26 @@
-// WAP to merge three number. E.g. a= 1, b= 2, c = 8 is 128
+// WAP to print the reverse of a 3 digit number
 
 #include<stdio.h>
 
 int main(){
-  int a, b, c, number = 0;
+  int number , revNumber = 0;
 
-  printf("Enter the value of A : ");
-  scanf("%d", &a);
-  printf("Enter the value of B : ");
-  scanf("%d", &b);
-  printf("Enter the value of C : ");
-  scanf("%d", &c);
+  printf("Enter the number : ");
+  scanf("%d", &number);
 
-  number += a; // 0+1 = 1
+  revNumber += number%10; 
+  number /= 10;
+  revNumber *= 10;
 
-  number *= 10; //10
-  number += b; //12
+  revNumber += number%10;
+  number /= 10;
+  revNumber *= 10;
 
-  number *= 10; //120
-  number += c; //3
+  revNumber += number%10;
+  number /= 10;
 
-  printf("The number is %d", number);
 
+  printf("Reverse Number is %d", revNumber);
+  
   return 0;
 }
