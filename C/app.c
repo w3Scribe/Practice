206e43f7b0fda539f0ff7f69da81ce441b89e57e
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-08-08 16:56:26 +0530

    chore: Add twoSum function to find indices of two numbers that sum up to a target

--- C/app.c
+++ C/app.c
@@ -1,6 +1,34 @@
+// two sum
 #include<stdio.h>
+#include<stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                if (result == NULL) {
+                    return NULL; // Memory allocation failed
+                }
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return NULL; // No solution found
+}
 
 int main(){
-  printf("Hello world");
+  int arr[] = {1,2,3};
+  int *indx = twoSum(arr, 7, 4);
+
+  if(indx != NULL){
+    printf("%d %d", indx[0], indx[1]);
+    free(indx);
+  } else{
+    printf("something went wrong!");
+  }
+
   return 0;
 }
