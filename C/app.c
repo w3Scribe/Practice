5cdc9eaaca8aebae3c301db2e65377b2c2af258c
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-08-08 17:07:23 +0530

    Refactor twoSum function to use macro for sum calculation

--- C/app.c
+++ C/app.c
@@ -1,34 +1,7 @@
-// two sum
 #include<stdio.h>
-#include<stdlib.h>
 
-int* twoSum(int* nums, int numsSize, int target) {
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
-                int* result = (int*)malloc(2 * sizeof(int));
-                if (result == NULL) {
-                    return NULL; // Memory allocation failed
-                }
-                result[0] = i;
-                result[1] = j;
-                return result;
-            }
-        }
-    }
-    return NULL; // No solution found
-}
+#define sum(a,b) (printf(#a" and "#b" sum is %d", a+b))
 
-int main(){
-  int arr[] = {1,2,3};
-  int *indx = twoSum(arr, 7, 1);
-
-  if(indx != NULL){
-    printf("%d %d", indx[0], indx[1]);
-    free(indx);
-  } else{
-    printf("something went wrong!");
-  }
-
-  return 0;
+void main(){
+  sum(1,35);
 }
