594f23ec6c6fa4addd9d7854b74b770097b3c68d
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-09-24 19:45:17 +0530

    Refactor reverseNumber function to handle negative numbers correctly

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,5 +1,5 @@
 const reverseNumber = (num : number) : number => {
   if(num.toString().length === 1) return num;
   const reverNumberStr = num.toString().split("").reverse().join("");
-  return Math.sign(num) * parseInt(reverNumberStr)
+  return Math.sign(num) * parseInt(reverNumberStr)      
 }
