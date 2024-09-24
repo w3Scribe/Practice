02060a412984bdd146da2b4fc0e9ecbf78e775f5
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-09-24 19:45:01 +0530

    Refactor FizzBuzz algorithm to add reverseNumber function for reversing a number

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,17 +1,5 @@
-type FizzBuzz = (n: number) => (string | number)[] | string;
-
-const fizzBuzz: FizzBuzz = (n) => {
-  if (n < 3 || typeof n !== "number") return "Invalid input";
-  const result: (number | string)[] = [];
-
-  for (let i = 1; i <= n; i++) {
-    if (i % 15 == 0) result.push("FizzBuzz");
-    else if (i % 3 == 0) result.push("Fizz");
-    else if (i % 5 == 0) result.push("Buzz");
-    else result.push(i);
-  }
-
-  return result;
-};
-
-console.log(fizzBuzz(1)); // [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+const reverseNumber = (num : number) : number => {
+  if(num.toString().length === 1) return num;
+  const reverNumberStr = num.toString().split("").reverse().join("");
+  return Math.sign(num) * parseInt(reverNumberStr)
+}
