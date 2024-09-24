e12aba01785913658107948836702258e5a6f987
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-09-24 19:36:44 +0530

    Refactor FizzBuzz algorithm to handle invalid input and improve code readability

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,22 +1,17 @@
-type FizzBuzz = (num: number) => (string | number)[];
+type FizzBuzz = (n: number) => (string | number)[] | string;
 
-const fizzBuzz: FizzBuzz = (num) => {
-  if (num < 3 || typeof num !== "number") return [];   // if num is less than 3 or not a number, return empty array
+const fizzBuzz: FizzBuzz = (n) => {
+  if (n < 3 || typeof n !== "number") return "Invalid input";
+  const result: (number | string)[] = [];
 
-  const result: (string | number)[] = [];
-
-  for (let i = 1; i <= num; i++) {
-    const condition_one = i % 3 === 0;
-    const condition_two = i % 5 === 0;
-
-    if (condition_one && condition_two) result.push("FizzBuzz");
-    else if (condition_one) result.push("Fizz");
-    else if (condition_two) result.push("Buzz");
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 == 0) result.push("FizzBuzz");
+    else if (i % 3 == 0) result.push("Fizz");
+    else if (i % 5 == 0) result.push("Buzz");
     else result.push(i);
   }
 
   return result;
 };
 
-const result = fizzBuzz(3);
-console.log(result);
+console.log(fizzBuzz(15)); // [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
