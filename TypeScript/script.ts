6f95443843358f8976478b1c85a2e36b2befdd7c
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-09-24 19:18:22 +0530

    Refactor FizzBuzz algorithm to handle item not found scenario and improve code readability

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,14 +1,22 @@
-type FizzBuzz = (num: number) => (number | string)[];
+type FizzBuzz = (num: number) => (string | number)[];
 
 const fizzBuzz: FizzBuzz = (num) => {
-  const store = [];
+ 
+
+  const result: (string | number)[] = [];
+
   for (let i = 1; i <= num; i++) {
-    const c1 = i % 3 == 0;
-    const c2 = i % 5 == 0;
-    store.push(c1 && c2 ? "fizzBuzz" : c1 ? "fizz" : c2 ? "Buzz" : i);
+    const condition_one = i % 3 === 0;
+    const condition_two = i % 5 === 0;
+
+    if (condition_one && condition_two) result.push("FizzBuzz");
+    else if (condition_one) result.push("Fizz");
+    else if (condition_two) result.push("Buzz");
+    else result.push(i);
   }
-  return store;
+
+  return result;
 };
 
-const result = fizzBuzz(100);
-console.log(result.filter((res) => res == "fizz").length);
+const result = fizzBuzz(3);
+console.log(result);
