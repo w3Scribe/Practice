cc5aa296f602733f0af6bc8fe73573be4a04bfa0
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-09-25 08:18:47 +0530

    trying to write the ArrayChunk alo

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,5 +1,23 @@
-const reverseNumber = (num: number): number => {
-  const numStr = num.toString();
-  if (numStr.length === 1) return num;
-  return Math.sign(num) * parseInt(numStr.split("").reverse().join(""));
+type ArrayChunk = (
+  arr: (number | string)[],
+  chunks: number
+) => (string | number)[] | (number | string)[][] | null;
+
+const arrayChunks: ArrayChunk = (arr, chunks) => {
+  const arrLen: number = arr.length;
+  if (arrLen <= 1 && chunks < 0) return arr;
+  let result: string | number | (number | string)[][] = [];
+
+  for (let i = 0; i < arrLen; i++) {
+    if (i % chunks === 0) {
+      const chunk = arr.splice(0, chunks);
+      result.push(chunk);
+    }
+  }
+
+  return result;
 };
+
+const result = arrayChunks([1, 2, 3, 4, 5], 2);
+
+console.log(result);
