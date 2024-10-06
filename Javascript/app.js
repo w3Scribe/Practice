1c96adc27516eb531473aeeded36c5813891f03f
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-10-06 18:41:15 +0530

    Add Animal class with methods and update README with Object prototype methods

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,15 +1,34 @@
 // Creating Class and Structure of the class
-class Person
-
-
-
 // Creating Instance of the class, Ojects , Properties and Methods
-// Creating Constructor for Object initiallization 
+// Creating Constructor for Object initiallization and passing values to the object
+
+
+class Animal {
+  constructor(name, sound){
+    this.name = name;
+    this.sound = sound;
+  }
+
+  makeSound(){
+    return this.sound;
+  }
+
+  getName(){
+    return this.name;
+  }
+}
+
+const dog = new Animal('Dog', 'Bark');
+const result = 'name' in dog;
+console.log(result);
+
+
+
+
 // Extending class and inheriting properties and methods
 // Access control ( public, private, protected )
 // Method overriding and method overloading 
 // Hiding Implementation details and showing only essential features
-
 
 
 
