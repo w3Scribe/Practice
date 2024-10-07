9e0edc05102ed56e66c97f46c4102540b86cca91
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-10-07 20:42:28 +0530

    Refactor Person class: simplify constructor and improve property accessors

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,39 +1,25 @@
-interface IPerson {
-  name: string;
-  age: number;
-}
-
 class Person {
-  private _name: string;
-  private _age: number;
-
-  constructor({ name, age }: IPerson) {
-    this._name = name;
-    this._age = age;
-  }
-
-  get name() : string {
-    return this._name;
-  }
-
-  get age() : number {
-    return this._age;
+  constructor(private __name: string, private __age: number) {}
+  get name() {
+    return this.__name;
   }
 
   set name(name: string) {
-    this._name = name;
+    this.__name = name;
+  }
+
+  get age() {
+    return this.__age;
   }
 
   set age(age: number) {
-    this._age = age;
+    this.__age = age;
   }
 }
 
-const person = new Person( {
-  age : 20,
-  name: 'John'
-});
 
-person.age = 21;
+const person = new Person('John', 30);
 
-console.log(person.age); 
+const result = person.name;
+
+console.log(result);
