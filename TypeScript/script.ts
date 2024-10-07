ff50f6817a4a3bf81208653cf8cd41a3d3905dff
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-10-07 20:33:34 +0530

    Refactor Animal class and enhance Person class with encapsulation and property accessors

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,17 +1,39 @@
-interface BankAccountProps {
+interface IPerson {
   name: string;
-  balance: number;
-  this : 
+  age: number;
 }
 
-type TBankAccount = {
-  new (name: string, number: number): BankAccountProps;
-};
+class Person {
+  private _name: string;
+  private _age: number;
 
-const BankAccount: TBankAccount = function ({
-  name,
-  balance,
-}: BankAccountProps) {
-  
+  constructor({ name, age }: IPerson) {
+    this._name = name;
+    this._age = age;
+  }
 
-} as unknown as TBankAccount;
+  get name() : string {
+    return this._name;
+  }
+
+  get age() : number {
+    return this._age;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  set age(age: number) {
+    this._age = age;
+  }
+}
+
+const person = new Person( {
+  age : 20,
+  name: 'John'
+});
+
+person.age = 21;
+
+console.log(person.age); 
