25ddb338bbab365f5a82dc37bbb60629cc6f5408
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-10-27 09:49:37 +0530

    Implement DoublyLinkedList class and related functionality; remove hello export

--- TypeScript/dist/script.js
+++ TypeScript/dist/script.js
@@ -1,2 +1,35 @@
-import { hello } from "./index";
-console.log(hello);
+"use strict";
+class ListNode {
+    value;
+    prevNode = null;
+    nextNode = null;
+    constructor(value) {
+        this.value = value;
+    }
+}
+class DoublyLinkedList {
+    head = null;
+    tail = null;
+    length = 0;
+    add(value) {
+        const newNode = new ListNode(value);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.nextNode = newNode;
+            newNode.prevNode = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    get size() {
+        return this.length;
+    }
+}
+const list = new DoublyLinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+console.log(list.size); // 3
