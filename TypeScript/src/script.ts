fede680a43a80c27d30dea0d2de555b458bdc350
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-12 22:32:15 +0530

    Enhance User interface with Address type and implement DeepPartial utility type for improved type handling

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,9 +1,29 @@
 // Partial
 
+interface Address {
+  street: string;
+  city: string;
+}
+
 interface User {
   name: string;
   age: number;
-  address : string;
+  address : Address;
 }
 
-type PartialUser = Partial<User>;
+// type PartialUser = Partial<User>; // This is not deep partial
+
+type DeepPartial<T> = {
+  [key in keyof T]? : T[key] extends object ? DeepPartial<T[key]> : T[key];
+}
+
+
+type PartialUser = DeepPartial<User>;
+
+const user: PartialUser = {
+  name: 'John',
+  address: {
+    street: '123 Street',
+    city: 'City'
+  }
+}
