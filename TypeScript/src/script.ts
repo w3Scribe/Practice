aa8d3da63b3f0fa2304674e0a70980ae172f915e
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-12 22:02:02 +0530

    Update .gitignore to exclude _pvt directory and clean up TypeScript examples with Omit utility type

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,47 +1,23 @@
-// omit, partial, readonly, record, pick, exclude, extract, nonNullable, returnType, instanceType, required, thisType, typeOf, unionize, intersect, promiseType, deepReadonly, deepRequired, deepNonNullable, deepPartial, deepExtract, deepExclude, deepOmit, deepPick, deepUnionize, deepIntersect, deepPromiseType, deepInstanceType, deepReturnType, deepTypeOf, deepKeys, deepMerge, deepMergeTwo, deepMergeThree, deepMergeFour, deepMergeFive, deepMergeSix, deepMergeSeven, deepMergeEight, deepMergeNine, deepMergeTen, deepMergeEleven, deepMergeTwelve, deepMergeThirteen, deepMergeFourteen, deepMergeFifteen, deepMergeSixteen, deepMergeSeventeen, deepMergeEighteen, deepMergeNineteen, deepMergeTwenty, deepMergeTwentyOne, deepMergeTwentyTwo, deepMergeTwentyThree, deepMergeTwentyFour, deepMergeTwentyFive, deepMergeTwentySix, deepMergeTwentySeven, deepMergeTwentyEight, deepMergeTwentyNine, deepMergeThirty, deepMergeThirtyOne, deepMergeThirtyTwo, deepMergeThirtyThree, deepMergeThirtyFour, deepMergeThirtyFive, deepMergeThirtySix, deepMergeThirtySeven, deepMergeThirtyEight, deepMergeThirtyNine, deepMergeForty, deepMergeFortyOne, deepMergeFortyTwo, deepMergeFortyThree, deepMergeFortyFour, deepMergeFortyFive, deepMergeFortySix, deepMergeFortySeven, deepMergeFortyEight, deepMergeFortyNine, deepMergeFifty, deepMergeFiftyOne, deepMergeFiftyTwo, deepMergeFiftyThree, deepMergeFiftyFour, deepMergeFiftyFive, deepMergeFiftySix, deepMergeFiftySeven, deepMergeFiftyEight, deepMergeFiftyNine, deepMergeSixty, deepMergeSixtyOne, deepMergeSixtyTwo, deepMergeSixtyThree, deepMergeSixtyFour, deepMergeSixtyFive, deepMergeSixtySix, deepMergeSixtySeven, deepMergeSixtyEight, deepMergeSixtyNine, deepMergeSeventy, deepMergeSeventyOne, deepMergeSeventyTwo, deepMergeSeventyThree, deepMergeSeventyFour, deepMergeSeventyFive, deepMergeSeventySix, deepMergeSevent
-
-// omit
-
-type A = {
-  a: string;
-  b: number;
-  c: boolean;
-};
-
-type B = Omit<A, 'a' | 'b'>;
-
-// partial
-
-type A1 = {
-  a: string;
-  b: number;
-  c: boolean;
-};
-
-type B1 = Partial<A1>;
-
-// readonly
-
-type A2 = {
-  a: string;
-  b: number;
-  c: boolean;
-};
-
-type B2 = Readonly<A2>;
-
-// record
-
-type A3 = 'a' | 'b' | 'c';
-
-type B3 = Record<A3, string>;
-
-const obj: B3 = {
-  a : 'a', // The number is not assignable to type 'string' if we try to assign a number to a
-  b : 'b',
-  c : 'c'
+interface Address {
+  street : string;
+  city : string;
+  country : string;
 }
 
-// pick
+interface Person {
+  name : string
+  age : number
+  address : Address
+}
 
+type PersonWithoutAddress = Omit<Person, "address">
 
+const person : Person = {
+  name: "John",
+  age: 30,
+  address: {
+    street: "123 Main St",
+    city: "New York",
+    country: "USA"
+  }
+}
