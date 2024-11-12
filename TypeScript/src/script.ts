a8f2d0194584e747a0c60e42a1368c28e4e58a06
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-12 21:18:52 +0530

    Refactor type definitions to simplify User interface and introduce Omit utility type

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,10 +1,12 @@
-// omit, partial, readonly, 
+// omit, partial, readonly, record, pick, exclude, extract, nonNullable, returnType, instanceType, required, thisType, typeOf, unionize, intersect, promiseType, deepReadonly, deepRequired, deepNonNullable, deepPartial, deepExtract, deepExclude, deepOmit, deepPick, deepUnionize, deepIntersect, deepPromiseType, deepInstanceType, deepReturnType, deepTypeOf, deepKeys, deepMerge, deepMergeTwo, deepMergeThree, deepMergeFour, deepMergeFive, deepMergeSix, deepMergeSeven, deepMergeEight, deepMergeNine, deepMergeTen, deepMergeEleven, deepMergeTwelve, deepMergeThirteen, deepMergeFourteen, deepMergeFifteen, deepMergeSixteen, deepMergeSeventeen, deepMergeEighteen, deepMergeNineteen, deepMergeTwenty, deepMergeTwentyOne, deepMergeTwentyTwo, deepMergeTwentyThree, deepMergeTwentyFour, deepMergeTwentyFive, deepMergeTwentySix, deepMergeTwentySeven, deepMergeTwentyEight, deepMergeTwentyNine, deepMergeThirty, deepMergeThirtyOne, deepMergeThirtyTwo, deepMergeThirtyThree, deepMergeThirtyFour, deepMergeThirtyFive, deepMergeThirtySix, deepMergeThirtySeven, deepMergeThirtyEight, deepMergeThirtyNine, deepMergeForty, deepMergeFortyOne, deepMergeFortyTwo, deepMergeFortyThree, deepMergeFortyFour, deepMergeFortyFive, deepMergeFortySix, deepMergeFortySeven, deepMergeFortyEight, deepMergeFortyNine, deepMergeFifty, deepMergeFiftyOne, deepMergeFiftyTwo, deepMergeFiftyThree, deepMergeFiftyFour, deepMergeFiftyFive, deepMergeFiftySix, deepMergeFiftySeven, deepMergeFiftyEight, deepMergeFiftyNine, deepMergeSixty, deepMergeSixtyOne, deepMergeSixtyTwo, deepMergeSixtyThree, deepMergeSixtyFour, deepMergeSixtyFive, deepMergeSixtySix, deepMergeSixtySeven, deepMergeSixtyEight, deepMergeSixtyNine, deepMergeSeventy, deepMergeSeventyOne, deepMergeSeventyTwo, deepMergeSeventyThree, deepMergeSeventyFour, deepMergeSeventyFive, deepMergeSeventySix, deepMergeSevent
 
+// omit
 
-interface User {
-  name : string
-  age : number
-  address : string
-}
+type A = {
+  a: string;
+  b: number;
+  c: boolean;
+};
 
-type UserGuest = Partial<User>
+type B = Omit<A, 'a' | 'b'>;
+
