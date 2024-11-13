23fe01401bf107f1f92291143de8bb13ceecf6a2
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-13 09:04:00 +0530

    Remove unused Person type and ReadOnly implementation from script.ts

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,30 +1,5 @@
 // Omit
-// Partial
+// Partial, deepPartial
 // readOnly , deepReadOnly
 
-type Person = Readonly<{
-  name: string;
-  age: number;
-  address: {
-    city: string;
-    country: string;
-  }
-}>
-
-
-type ReadOnly<T> = {
-  readonly [key in keyof T] : T[key] extends object ? ReadOnly<T[key]> : T[key]
-}
-
-
-const PersonOne: ReadOnly<Person> = {
-  name: "John",
-  age: 30,
-  address: {
-    city: "New York",
-    country: "USA"
-  }
-}
-
-PersonOne.address.city = 'ere'
 
