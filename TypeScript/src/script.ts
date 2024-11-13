404cd052edabad84ff816c087c2eabfc77fe7042
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-13 09:03:20 +0530

    Add deepReadOnly utility type and define Person type with ReadOnly implementation

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,4 +1,30 @@
 // Omit
 // Partial
-// readOnly
+// readOnly , deepReadOnly
+
+type Person = Readonly<{
+  name: string;
+  age: number;
+  address: {
+    city: string;
+    country: string;
+  }
+}>
+
+
+type ReadOnly<T> = {
+  readonly [key in keyof T] : T[key] extends object ? ReadOnly<T[key]> : T[key]
+}
+
+
+const PersonOne: ReadOnly<Person> = {
+  name: "John",
+  age: 30,
+  address: {
+    city: "New York",
+    country: "USA"
+  }
+}
+
+PersonOne.address.city = 'ere'
 
