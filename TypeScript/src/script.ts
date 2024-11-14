3641189ba3295430552ad6772f4e2272fe1383f1
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-14 20:40:34 +0530

    Add RequiredKeys utility type to extract required keys from Person interface

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -8,3 +8,11 @@ interface Person {
     address: string;
   }
 }
+
+
+type RequiredKeys<Type> = {
+  [Key in keyof Type] -?: Type[Key] extends object ? RequiredKeys<Type[Key]> : Key; 
+}[keyof Type];
+
+type RequiredKeysOfPerson = RequiredKeys<Person>;
+
