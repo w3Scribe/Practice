278a42cd844d3f186bf0758d260b585a424b2ba2
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-15 22:31:52 +0530

    Refactor Fn type signature for consistency and clarity

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,7 +1,8 @@
-type Fn = (a : number, b : object) => boolean;
+type Fn = (a: number, b: object) => boolean;
 
 type ReturnDataType<T> = T extends (...args: any[]) => infer Rvalue ? Rvalue : never;
-type ParamsFn<T> = T extends (...args : infer Params) => any ? Params : never;
+type ParamsFn<T> = T extends (...args: infer Params) => any ? Params : never;
 
 type ReturnTypeOfFn = ReturnDataType<Fn>; // number
 type ParamsTypeOfFn = ParamsFn<Fn>; // [number, string]
+
