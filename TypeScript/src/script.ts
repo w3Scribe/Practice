2bee89ae63cc4ef0f72d830ef07e6fac62b4f6fb
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-15 21:38:34 +0530

    Add ArrayWithLength type and example usage in script.ts

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,4 +1,6 @@
 // usage of is keyword in typescript
 // typeof, instanceof, in, as, satisfies, is, type guards,, 
 
+type ArrayWithLength<Length extends number, DataType> = [DataType, ...DataType[]] & { length: Length };
 
+const arr : ArrayWithLength<4, number | string> = ['one', 2, 'three', 4];
