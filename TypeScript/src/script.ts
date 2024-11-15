7142dfd077edf1d3c792f9c50b879421543a6e58
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-15 20:04:07 +0530

    Add Tuple type and RGB constant with type validation

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -2,6 +2,10 @@
 // typeof, instanceof, in, as, satisfies, is, type guards,, 
 
 
+type Tuple<Length, DataType, Acc extends DataType[] = []> =
+  Acc["length"] extends Length ? Acc : Readonly<Tuple<Length, DataType, [DataType, ...Acc]>>;
 
+type RGB = Tuple<3, number>;
 
+const rgb = [255, 255, 255] satisfies RGB; 
 
