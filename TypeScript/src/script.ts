34d17d4cfbfe781f5aeea316d143acba4fdc9e6a
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-15 16:08:33 +0530

    Add User and Address interfaces with a function to log user details

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,1 +1,30 @@
-type Tuple<L, T, A extends T[] = []> = A["length"] extends L ? A : Readonly<Tuple<L, T, [T, ...A]>>;
+interface Address {
+  street: string;
+  city: string;
+}
+
+interface User {
+  name: string;
+  age: number;
+  address : Address;
+}
+
+type AddUserDetails<T extends User> = (userDetails : T) => void;
+
+
+const addUserDetails : AddUserDetails<User> = (userDetails) => {
+  console.log(userDetails);
+}
+
+
+const user = {
+  name: 'John',
+  age: 30,
+  address: {
+    street: '123 Main St',
+    city: 'New York'
+  }
+}
+
+
+addUserDetails(user);
