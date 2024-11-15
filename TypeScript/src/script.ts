d183a4090c201822ac92c9dbeefcba953a939efd
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-15 19:58:37 +0530

    Remove Dog and Cat interfaces along with associated animal handling logic

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,26 +1,6 @@
 // usage of is keyword in typescript
 // typeof, instanceof, in, as, satisfies, is, type guards,, 
 
-interface Dog {
-  bark(): void;
-}
-
-interface Cat {
-  meow(): void;
-}
-
-type AnimalType = Dog | Cat;
-
-
-function Animal<T extends AnimalType>(animal: T){
-  if ('bark' in animal) {
-    animal.bark();
-  } else {
-    animal.meow();
-  }
-}
-
-
 
 
 
