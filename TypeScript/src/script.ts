6b251a0090b51149a69da4b1f0af2fb22922cf68
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-15 22:24:52 +0530

    Update Fn type signature and introduce ParamsFn type for parameter extraction

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,5 +1,7 @@
-type Fn = () => boolean;
+type Fn = (a : number, b : object) => boolean;
 
 type ReturnDataType<T> = T extends (...args: any[]) => infer Rvalue ? Rvalue : never;
+type ParamsFn<T> = T extends (...args : infer Params) => any ? Params : never;
 
 type ReturnTypeOfFn = ReturnDataType<Fn>; // number
+type ParamsTypeOfFn = ParamsFn<Fn>; // [number, string]
