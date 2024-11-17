a8133d246f61b9665b810498c9fbddcc93973667
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-17 12:56:26 +0530

    Remove DoublyLinkedList implementation and associated utility functions; add TypeScript type definitions for Tailwind CSS colors and spacing

---
+++ TypeScript/src/@types/tw.ts
@@ -0,0 +1,104 @@
+// COLORS
+type ColorName =
+  | 'white'
+  | 'black'
+  | 'slate'
+  | 'gray'
+  | 'zinc'
+  | 'neutral'
+  | 'stone'
+  | 'red'
+  | 'orange'
+  | 'amber'
+  | 'yellow'
+  | 'lime'
+  | 'green'
+  | 'emerald'
+  | 'teal'
+  | 'cyan'
+  | 'sky'
+  | 'blue'
+  | 'indigo'
+  | 'violet'
+  | 'purple'
+  | 'fuchsia'
+  | 'pink'
+  | 'rose';
+
+type ColorShade =
+  | '50'
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900'
+  | '950';
+
+export type Color =
+  | `${'bg' | 'text'}-${ColorName}-${ColorShade}`
+  | `${'bg' | 'text'}-${'current' | 'inherit' | 'transparent'}`;
+
+// SPACING from tailwind css
+
+type Space =
+  | '0'
+  | 'px'
+  | '0.5'
+  | '1'
+  | '1.5'
+  | '2'
+  | '2.5'
+  | '3'
+  | '3.5'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11'
+  | '12'
+  | '14'
+  | '16'
+  | '20'
+  | '24'
+  | '28'
+  | '32'
+  | '36'
+  | '40'
+  | '44'
+  | '48'
+  | '52'
+  | '56'
+  | '60'
+  | '64'
+  | '72'
+  | '80'
+  | '96'
+  // Percentage values
+  | '1/2'
+  | '1/3'
+  | '2/3'
+  | '1/4'
+  | '2/4'
+  | '3/4'
+  | '1/5'
+  | '2/5'
+  | '3/5'
+  | '4/5'
+  | '1/6'
+  | '2/6'
+  | '3/6'
+  | '4/6'
+  | '5/6'
+  | 'full'
+  | 'auto';
+
+export type Spacing =
+  | `${'p' | 'm'}-${Space}`
+  | `${'p' | 'm' | 'space'}-${'x' | 'y' | 't' | 'r' | 'b' | 'l'}-${Space}`;
