b806ba15461145d005c84d4a89fc1b02bb89a42c
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-21 00:05:53 +0530

    Remove unused Arry type and commented-out First utility type implementations from script.ts for cleaner code.

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,5 +1,1 @@
-type Arry = ['a', 'b', 'c'];
-
-// type First<T extends PropertyKey[]> = T[number] extends never ? never : T[0];
-// type First<T extends PropertyKey[]> = T extends [infer F, ...infer Rest] ? F : never;
-// type First<T extends PropertyKey[]> = T extends [] ? never : T[0];
+type Fruits = ['apple']
