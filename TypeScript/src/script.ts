37c88f79bf905785f0e481d145a96f3d2dfa993b
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-20 21:49:40 +0530

    Add TypeScript utility types for fruits and tuple conversion; define Fruits type and create TupleToObject for dynamic object generation.

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,2 +1,7 @@
-const RGB : Tuple<3,number> = [0,0,0]
+type Fruits = ['apple', 'banana', 'cherry', 'date'];
 
+type TupleToObject<T extends any[], prefix extends string = ''> = {
+  [K in T[number] as `${prefix}${K}`] : K
+}
+
+type FruitObject = TupleToObject<Fruits, 'fruit_'>;
