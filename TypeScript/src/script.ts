3f5c830ca99f846b713325570160a6d7c18fa4d3
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-20 23:52:37 +0530

    Refactor TypeScript types in script.ts; remove old tuple and object types, introduce new array type and First utility type for extracting the first element.

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,19 +1,4 @@
-type A = [0,1, 2, 3, 4, 'false'];
-type B = {
-  0: '0';
-  1: '1';
-  2: '2';
-  3: '3';
-  4: '4';
-  'false' : 'false'
-};
+type arry = ['hello', 'world', 'foo', 'bar'];
 
-type TupleToObject<T extends any[]> = {
-  [P in T[number]]: `${P}`
-}
-
-
-type C = TupleToObject<A>
-
-type TestOne = IsEqual<A,B>
-type TestTwo = IsEqual<B,C>
+type First<T extends PropertyKey[]> = T extends [infer F, ...infer rest] ? F : never;
+type Test = First<arry>
