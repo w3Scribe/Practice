7b932a00b2a7a2eb6c7bb9361b2bea53052f18fd
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-20 23:59:22 +0530

    Refactor First utility type in script.ts; comment out previous implementations and provide alternative definitions for better clarity.

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,4 +1,10 @@
 type arry = ['hello', 'world', 'foo', 'bar'];
 
-type First<T extends PropertyKey[]> = T extends [infer F, ...infer rest] ? F : never;
-type Test = First<arry>
+// type First<T extends PropertyKey[]> = T extends [infer F, ...infer rest] ? F : never;
+// type Test = First<arry>
+
+// type First<T extends PropertyKey[]> = T[number] extends never ? never : T[0];
+// type Test = First<arry>
+
+// type First<T extends PropertyKey[]> = T extends [] ? never : T[0];
+// type Test = First<arry>
