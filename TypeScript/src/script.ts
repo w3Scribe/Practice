085582e008b7b0d2b335c7b4843ee7a82ef9e903
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-21 18:33:24 +0530

    Add MyParameters utility type to infer parameter types from a function in script.ts

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,6 +1,13 @@
-type FruitsName = ['apple', 'bananas', 'mango']
+const foo = (arg1: string, arg2: number): void => {};
+const bar = (arg1: boolean, arg2: { a: 'A' }): void => {};
+const baz = (): void => {};
 
+type test = MyParameters<typeof bar, [boolean, { a: 'A' }]>;
 
-type Unshift<T extends any[], U> = T extends [...infer Rest] ? [U, ...Rest] : never
-
-type Test = Unshift<FruitsName, "orange">
+type MyParameters<T extends (...args: any[]) => any, ParamsType extends any[] = []> = T extends (
+  ...args: infer P
+) => any
+  ? P extends ParamsType
+    ? true
+    : false
+  : false;
