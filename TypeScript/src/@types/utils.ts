93f6bfd8e47f1be6a1574e77be32f5eaf03a25ae
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-21 21:37:09 +0530

    Add ReadonlyProps and OptionalProps utility types; refactor UserDetails to Todo interface in script.ts

--- TypeScript/src/@types/utils.ts
+++ TypeScript/src/@types/utils.ts
@@ -6,6 +6,7 @@ declare global {
    */
   type IsEqual<A, B> = A extends B ? (B extends A ? true : false) : false;
 
+
   /**
    * Constructs a tuple type of length `L` with elements of type `T`.
    * @template L - The length of the tuple
@@ -23,6 +24,29 @@ declare global {
       ? Readonly<Acc>
       : Acc
     : Tuple<L, T, R, [...Acc, T]>;
+
+
+  /**
+   * ReadonlyProps makes the specified properties of an object readonly
+   * @template T - The type to make readonly
+   * @template P - The properties to make readonly
+   * @example ReadonlyProps<{ a: string, b: number }, 'a'> => { readonly a: string, b: number }
+   */
+  type ReadonlyProps<T, P extends keyof T = keyof T> = {
+    readonly [K in P]: T[K];
+  } & Omit<T, P>;
+
+
+  /**
+   * OptionalProps makes the specified properties of an object optional
+   * @template T - The type to make optional
+   * @template K - The properties to make optional
+   * @example OptionalProps<{ a: string, b: number }, 'a'> => { a?: string, b: number }
+   */
+  type OptionalProps<T, K extends keyof T = keyof T> = {
+    [P in K]?: T[P];
+  } & Omit<T, K>;
+
 }
 
 export {};
--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,10 +1,7 @@
-interface UserDetails {
-    name: string;
-    age: number;
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
 }
 
-type MyOmit<T, K extends  keyof T> = {
-   [P in Exclude<keyof T, K>]: T[P]
-}
-
-type Test = MyOmit<UserDetails, 'name'>;
+type OptionalTodo = OptionalProps<Todo>;
