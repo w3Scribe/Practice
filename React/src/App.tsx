c8237a9473d92b973110dc4ff647c93a88ee5bf7
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-23 17:17:40 +0530

    Refactor App component to remove List usage and directly render content

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,14 +1,10 @@
 import { FC, Fragment } from "react";
-import List from "./component/list/list";
 
 const App: FC = () => {
   return (
     <Fragment>
-      <List>
-        <List.Header className="bg-slate-900 text-slate-100 font-semibold p-5">Heading</List.Header>
-        <List.Item>hello world</List.Item>
-      </List>
-      <ul className=""></ul>
+      hello world
+      <h1>hello world</h1>
     </Fragment>
   );
 };
