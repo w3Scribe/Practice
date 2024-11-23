b15988723a809571e619f194ca015d4075dfc59f
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-23 17:22:45 +0530

    Replace static content in App component with List component for dynamic rendering

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,10 +1,12 @@
 import { FC, Fragment } from "react";
+import List from "./component/list/list";
 
 const App: FC = () => {
   return (
     <Fragment>
-      hello world
-      <h1>hello world</h1>
+      <List className="bs-slate-900">
+        <List.Item>list item</List.Item>
+      </List>
     </Fragment>
   );
 };
