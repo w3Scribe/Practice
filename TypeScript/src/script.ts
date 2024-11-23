792b5d06790c4ea039b25bf078822b9dfc46e544
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-23 17:51:13 +0530

    Refactor type definitions by removing Details interface and adding If utility type

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,18 +1,3 @@
-interface Details {
-  name: string;
-  age: number;
-  location: string;
-  other: {
-    email: string;
-    phone: string;
-    hobbies: {
-      name: string;
-      duration: number;
-    };
-  };
-}
+type If<C extends boolean, T, F> = C extends true ? T : F;
 
-type DeepReadonly<T> = T extends object ? { readonly [P in keyof T]: DeepReadonly<T[P]> } : T;
-
-
-type Test = DeepReadonly<Details>;
+type A = If<true, 'a', 'b'>; // 'a'
