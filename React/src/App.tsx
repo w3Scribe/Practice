ddc8fe8d5a3556922ffdf2cecc28d856974b5492
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-24 22:30:07 +0530

    Remove hardcoded fruit list from App component and replace with a placeholder list item

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,17 +1,13 @@
 import { FC } from "react";
 import List from "./component/list/list";
 
-const Fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "quince", "raspberry", "strawberry", "tangerine", "ugli", "watermelon"];
-
 const App: FC = () => {
   return (
     <div className="p-5 bg-slate-900">
       <List>
-        {Fruits.map((fruit, index) => (
-          <List.Item key={index} className="text-white">
-            {fruit}
-          </List.Item>
-        ))}
+        <List.Item className="bg-slate-900 bg-slate-900">
+          list item
+        </List.Item>
       </List>
     </div>
   );
