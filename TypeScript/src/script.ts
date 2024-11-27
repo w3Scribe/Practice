975ba35901a9cd595825a661b20da56c780c6502
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-11-27 18:01:39 +0530

    Define IUserDetails type for user details structure and update produce function to use the new type

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,20 +1,27 @@
 import { produce } from 'immer';
 
-const userDetails = {
+type IUserDetails = {
+  name: string;
+  age: number;
+  email: string;
+  address: {
+    city: string;
+    country: string;
+  };
+};
+
+const userDetails: IUserDetails = {
   name: 'John',
   age: 25,
   email: 'abc@gmail.com',
   address: {
-    city: 'New York',
+    city: 'New York',     
     country: 'USA',
   },
 };
 
-
-
-const result = produce(userDetails, (draft) => {
-  draft.age = 26
+const result = produce<IUserDetails>(userDetails, (draft) => {
+  draft.age = 26;
 });
 
 console.log(result);
-
