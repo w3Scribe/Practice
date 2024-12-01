d1d99bbef13b66841eaa156c95dfac8ca6a7f230
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-12-01 10:24:04 +0530

    Add Exclude type for filtering out specific types from a union

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,7 +1,13 @@
-type tesla = ['tesla', 'model 3', 'model X', 'model Y'];
+type Exclude<T extends unknown, U extends T | PropertyKey | unknown = T> = T extends U ? never : T;
 
-// type TuppleLen<T extends any[]> = T  extends any[] ? T['length'] : never;
-// type TuppleLen<T extends any[]> = T['length'];
-// type TuppleLen<T extends any[]> = T extends { length: infer L } ? L : never;
+type MixedTypes = 'apple' | 'banana' | 1 | 2 | (() => void) | { key: string } | false;
 
-type teslaLength = TuppleLen<tesla>; // expected 4
+type Test1 = Exclude<MixedTypes, 'apple' | 'banana'>; // 'apple' | 'orange'
+type Test2 = Exclude<MixedTypes, 1 | 2>; // 'apple' | 'banana' | (() => void) | { key: string } | false
+type Test3 = Exclude<MixedTypes, false>; // 'apple' | 'banana' | 1 | 2 | (() => void) | { key: string }
+type Test4 = Exclude<MixedTypes, (() => void) | { key: string }>; // 'apple' | 'banana' | 1 | 2 | false
+
+type Test5 = Exclude<MixedTypes, string> // false | {key: string;} | 1 | 2 | (() => void)
+type Test6 = Exclude<MixedTypes, string | number> // false | {key: string;} | (() => void)
+type Test7 = Exclude<MixedTypes, string | number | boolean> // {key: string;} | (() => void)
+type Test8 = Exclude<MixedTypes, Function> // {key: string;}
