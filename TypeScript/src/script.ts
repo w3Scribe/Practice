e748b4ee50b5ae0845cc4773a51c9831d187dd3e
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-12-05 20:07:06 +0530

    Add Unshift utility type for array concatenation; replace Push type definition

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,7 +1,6 @@
 type arr1 = ['a', 'b', 'c'];
 type arr2 = [3, 2, 1];
 
-
-type Push<A1 extends Primitive[], A2 extends Primitive[]> = [...A1, ...A2];
-
-type Test = Push<arr1, arr2>
+type Unshift<A1 extends Primitive[], A2 extends Primitive> = A2 extends Primitive[]
+  ? [...A2, ...A1]
+  : [A2, ...A1];
