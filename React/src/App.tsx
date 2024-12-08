bdcabfa1f9bc15b3c7adf6e7504db12ca404696e
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-12-08 11:27:12 +0530

    Enhance styling of the header in App component with additional padding and outline

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -3,7 +3,7 @@ import { type FC , Fragment } from 'react';
 const App: FC = () => {
   return (
     <Fragment>
-      <h1 className='bg-amber-300'>Hello World!</h1>
+      <h1 className='bg-amber-300 underline p-4  outline-2 outline-amber-500 m-5'>Hello World!</h1>
     </Fragment>
   );
 };
