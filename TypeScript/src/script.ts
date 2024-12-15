89b785ff67a5dbc47d8760f6efc168e065377a66
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-12-15 19:35:09 +0530

    Simplify TLen type definition to directly access tuple length

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,5 +1,5 @@
 type Fruits = ['apple', 'banana', 'cherry'];
 
-type TLen<T extends any[]> = T extends { length: infer L } ? L : never;
+type TLen<T extends any[]> = T['length'];
 
 type Len = TLen<Fruits>; // expected 3
