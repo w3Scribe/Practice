412110b24992aa1cf212ffa8f3f2d377e6cbf514
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-12-22 19:33:04 +0530

    Enhance form styling and validation messages in App component; update form layout and input constraints

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -6,15 +6,23 @@ import twc from "./lib/twc";
 
 const style = twc({
   section: "bg-gray-900 min-w-full min-h-dvh grid place-items-center",
-  form: "w-60 p-2 bg-gray-800 border border-gray-700 rounded-sm flex flex-col justify-center gap-y-1",
+  form: "w-60 p-4 bg-gray-800 border border-gray-700 rounded-sm flex flex-col justify-center gap-y-3",
   formHeading: "text-center text-gray-50 font-bold mb-2 text-xl",
   input:
     "w-full py-1.5 px-1 bg-gray-600/40 outline-none border border-gray-500 text-gray-50",
 });
 
 const FormSchema = z.object({
-  username: z.string().trim().min(4),
-  password: z.string().trim().min(8),
+  username: z
+    .string()
+    .trim()
+    .min(4, "Username must be at least 4 characters")
+    .max(20, "Username must be at most 20 characters"),
+  password: z
+    .string()
+    .trim()
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must be at most 20 characters"),
 });
 
 type FromInputs = z.infer<typeof FormSchema>;
@@ -31,7 +39,7 @@ const App: FC = () => {
   return (
     <Fragment>
       <section className={style.section}>
-        <form className={style.form} onSubmit={submitFn}>
+        <form className={style.form} onChange={submitFn}>
           <h1 className={style.formHeading}>Login Form</h1>
           <Input form={FromHook} fieldName="username" className={style.input} />
           <Input
@@ -42,7 +50,7 @@ const App: FC = () => {
           />
           <button
             type="submit"
-            className={style.input + " bg-blue-500 hover:bg-blue-600"}
+            className={style.input + " bg-blue-500 hover:bg-blue-600 my-2"}
           >
             Submit
           </button>
