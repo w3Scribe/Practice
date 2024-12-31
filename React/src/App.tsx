bd108bed0007042048242b6f4fa30b0a9be68286
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-12-31 19:29:24 +0530

    Remove CompA and counter context; integrate motion for animation in App component

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,32 +1,12 @@
-import { type FC, Fragment, useState } from "react";
-
-import CompA from "./components/CompA";
-import CounterCxt from "./context/counterContext";
-
+import { motion } from 'motion/react';
+import { type FC , Fragment } from 'react';
 const App: FC = () => {
-  const [count, setCount] = useState<CounterState>(0);
-
-  const counterFn: TCounterFn = (action) => {
-    switch (action.type) {
-      case "Increment":
-        setCount(action.payload ? count + action.payload : count + 1);
-        break;
-      case "Decrement":
-        setCount(action.payload ? count - action.payload : count - 1);
-        break;
-      case "Reset":
-        setCount(0);
-        break;
-      default:
-        throw new Error("something went wrong !");
-    }
-  };
-
   return (
     <Fragment>
-      <CounterCxt value={{ count, counterFn }}>
-        <CompA />
-      </CounterCxt>
+      <motion.div animate={{ x: 100 }} transition={{ duration: 1 }} className='bg-amber-400 size-28'>
+        <h1>React Motion</h1>
+        <p>React Motion is a library for animations in React.</p>
+      </motion.div>
     </Fragment>
   );
 };
--- React/src/context/counterContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const CounterCxt = createContext<TCounterCtx>({
-  count: 0,
-  counterFn: () => {},
-});
-
-export default CounterCxt;
