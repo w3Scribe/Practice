f25ac2f11d7c8f9cc3a0975620d9c028aab01ee8
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-03 20:21:42 +0530

    Add type utilities for string length comparison and character constraints

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,5 +1,18 @@
-export const twc = <T extends Record<string, string>>(classes: T): T => {
-  return classes;
-};
+type Lenght<T extends string, CharCount extends number[] = []> = T extends `${string}${infer Rest}`
+  ? Lenght<Rest, [...CharCount, 0]>
+  : CharCount['length'];
 
-export default twc;
+
+type Compaare<First extends number, Second extends number, Result extends number[] = []> = 
+  First extends Second ? 'Equal' : 
+  First extends Result['length'] ? 'Less' : 
+  Second extends Result['length'] ? 'Greater' :
+  Compaare<First, Second, [...Result, 0]>;
+
+
+type MaxChar<T extends string, Max extends number = 0> = Compaare<Lenght<T>, Max> extends 'Equal' | 'Less' ? T : never;
+type MinChar<T extends string, Min extends number = 0> = Compaare<Lenght<T>, Min> extends 'Equal' | 'Greater' ? T : never;
+
+
+type TestMax = MaxChar<'a' , 5>; // 'ccc'
+type TestMin = MinChar<'ar' , 2>; // 'a'
