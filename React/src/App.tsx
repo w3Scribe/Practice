7e077a99d4370c93eb38a50853c668208463f2bf
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-12 13:14:24 +0530

    Update App component styles to include h2 header and fix formatting in twc utility; mark backend subproject as dirty

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,9 +2,9 @@ import { type FC } from 'react';
 import { twc } from './utils';
 
 const STYLE = twc({
-  h1 : 'text-center p-4 text-4xl font-bold text-blue-500'
+  h1: 'text-center p-4 text-4xl font-bold text-blue-500',
+  h2: 'text-center p-4 text-3xl font-bold text-blue-500',
 });
-
 
 const App: FC = () => {
   return (
