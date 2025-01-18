5a76012f9f7aad7a314876e3ed94a115a44aa6be
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-18 20:20:36 +0530

    Mark backend subproject as dirty; refactor TypeScript types for improved clarity and structure

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,37 +1,8 @@
-type NumToTupple<T extends number, Result extends number[] = []> = T extends Result['length']
-  ? Result
-  : NumToTupple<T, [...Result, 0]>;
+// TLen
+type TLen<T extends any[]> = T['length'];
 
-type StrToTupple<T extends any, Result extends number[] = []> = T extends `${string}${infer _}`
-  ? StrToTupple<_, [...Result, 0]>
-  : Result;
-
-type NSTupple<T extends number | string> = T extends string
-  ? [...StrToTupple<T>]
-  : T extends number
-    ? [...NumToTupple<T>]
-    : never;
-
-type Add<N1 extends number | string, N2 extends number | string> = [
-  ...NSTupple<N1>,
-  ...NSTupple<N2>,
-];
-
-type Substract<
-  T extends number | string,
-  S extends number | string,
-  Result extends (number | string)[] = NSTupple<T>,
-> = Result extends [...NSTupple<S>, ...infer _] ? _ : never;
-
-type Multiply<N1 extends number | string, N2 extends number | string, Result extends any[] = []> =
-  NSTupple<N2> extends [infer FArr, ...infer RArr]
-    ? Multiply<N1, RArr['length'], [...Result, ...NSTupple<N1>]>
-    : Result;
-
-type HexCodeMap = {
-  '1': 1
-  '2': 2
-  '3': 3
-  
-}
+// NumToTupple
+type NumToTupple<N extends number, R extends any[] = []> = R['length'] extends N
+  ? R
+  : NumToTupple<N, [...R, R['length']]>;
 
