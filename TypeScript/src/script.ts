dc5dbfdaac18ea30e629eb8f02cd0b2ced7daa6a
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-18 18:45:38 +0530

    Update backend subproject status to dirty; enhance TypeScript types with NSTupple, Substract, and Multiply; add HexCodeMap type

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -2,15 +2,36 @@ type NumToTupple<T extends number, Result extends number[] = []> = T extends Res
   ? Result
   : NumToTupple<T, [...Result, 0]>;
 
-type StrToTupple<T extends any, Result extends string[] = []> = T extends `${infer F}${infer _}`
-  ? StrToTupple<_, [...Result, F]>
+type StrToTupple<T extends any, Result extends number[] = []> = T extends `${string}${infer _}`
+  ? StrToTupple<_, [...Result, 0]>
   : Result;
 
-type BothEx<T extends any, N extends any> = T extends N ? T : never;
+type NSTupple<T extends number | string> = T extends string
+  ? [...StrToTupple<T>]
+  : T extends number
+    ? [...NumToTupple<T>]
+    : never;
 
-type Add<N1 extends number | string, N2 extends number | string> =
-  BothEx<N1, N2> extends number
-    ? [...NumToTupple<N1>, ...NumToTupple<N2>]
-    : [...StrToTupple<N1>, ...StrToTupple<N2>];
+type Add<N1 extends number | string, N2 extends number | string> = [
+  ...NSTupple<N1>,
+  ...NSTupple<N2>,
+];
 
+type Substract<
+  T extends number | string,
+  S extends number | string,
+  Result extends (number | string)[] = NSTupple<T>,
+> = Result extends [...NSTupple<S>, ...infer _] ? _ : never;
+
+type Multiply<N1 extends number | string, N2 extends number | string, Result extends any[] = []> =
+  NSTupple<N2> extends [infer FArr, ...infer RArr]
+    ? Multiply<N1, RArr['length'], [...Result, ...NSTupple<N1>]>
+    : Result;
+
+type HexCodeMap = {
+  '1': 1
+  '2': 2
+  '3': 3
+  
+}
 
