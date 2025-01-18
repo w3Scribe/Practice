2ac491158a07bb435b87413e8ca9afa41e37c948
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-18 20:34:23 +0530

    Remove demo-encore-app subproject; enhance TypeScript types with Add and Multiply utilities

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -2,7 +2,14 @@
 type TLen<T extends any[]> = T['length'];
 
 // NumToTupple
-type NumToTupple<N extends number, R extends any[] = []> = R['length'] extends N
-  ? R
-  : NumToTupple<N, [...R, R['length']]>;
+type NumToTupple<N extends number, R extends any[] = []> =
+  TLen<R> extends N ? R : NumToTupple<N, [...R, R['length']]>;
 
+// Add
+type Add<F extends number, S extends number> = TLen<[...NumToTupple<F>, ...NumToTupple<S>]>;
+
+// Multiply
+type Multiply<F extends number, S extends number, R extends any[] = []> =
+  NumToTupple<S> extends [infer N, ...infer SRest]
+    ? Multiply<F, TLen<SRest>, [...R, ...NumToTupple<F>]>
+    : TLen<R>;
