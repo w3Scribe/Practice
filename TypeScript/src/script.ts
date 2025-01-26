6747e98502205793830873f3e12b7e0549b81316
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-26 14:10:36 +0530

    Add @langchain/google-genai dependency and implement type swapping utility

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,1 +1,25 @@
-console.log("Hello, World");
+type SwapType = {
+  string: number;
+  number: string;
+  true: false;
+  false: true;
+  undefined: null;
+  null: undefined;
+  'string[]': number[];
+  'number[]': string[];
+};
+
+type Swap<T extends keyof SwapType> = SwapType[T];
+
+type SwapFn<T> = {
+  [K in keyof T]: T[K] extends keyof SwapType ? Swap<T[K]> : T[K];
+};
+
+type Original = {
+  age: number;
+  name: string;
+  active: boolean;
+  friends: string[];
+};
+
+type Swapped = SwapFn<Original>;
