612e950d8dd86e0078587923e5542c960bbc7f85
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-28 21:26:48 +0530

    Refactor OPaths type for improved object path resolution; enhance handling of arrays and nested objects

--- TypeScript/src/@types/global.d.ts
+++ TypeScript/src/@types/global.d.ts
@@ -6,9 +6,12 @@ type Draft<T extends object> = {
   -readonly [K in keyof T]: T[K] extends object ? Draft<T[K]> : T[K];
 };
 
-type OPaths<T extends object, R extends string = ''> = 
-  T extends object? 
-  T extends Array<infer U>?
-  U extends object ?
-  OPaths<U> : never :
-  { [K in keyof T] : K | `${K}.${OPaths<T[K]>}` }[keyof T] : never;
+type OPaths<T> = T extends object
+  ? {
+      [K in keyof T]: K extends string
+        ? T[K] extends Array<infer U>
+          ? `${K}` | `${K}[${number}]` | (U extends object ? `${K}[${number}].${OPaths<U>}` : never)
+          : `${K}` | (T[K] extends object ? `${K}.${OPaths<T[K]>}` : never)
+        : never;
+    }[keyof T]
+  : never;
