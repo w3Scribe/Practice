9292700e909e7ff3ad78bf117ac3a7db2d08db9f
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-28 20:58:20 +0530

    Add OPaths type for recursive object path resolution; introduce TestPaths type for Example

--- TypeScript/src/@types/global.d.ts
+++ TypeScript/src/@types/global.d.ts
@@ -6,3 +6,9 @@ type Draft<T extends object> = {
   -readonly [K in keyof T]: T[K] extends object ? Draft<T[K]> : T[K];
 };
 
+type OPaths<T extends object, R extends string = ''> = 
+  T extends object? 
+  T extends Array<infer U>?
+  U extends object ?
+  OPaths<U> : never :
+  { [K in keyof T] : K | `${K}.${OPaths<T[K]>}` }[keyof T] : never;
--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -11,4 +11,4 @@ type Example = {
 };
 
 
-
+type TestPaths = OPaths<Example>;
