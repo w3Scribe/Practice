0ea4ce92a38dd64c26bdca257fef0df54f0d9113
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-28 21:30:36 +0530

    Remove OPaths type definition from global types; streamline type declarations

--- TypeScript/src/@types/global.d.ts
+++ TypeScript/src/@types/global.d.ts
@@ -6,12 +6,3 @@ type Draft<T extends object> = {
   -readonly [K in keyof T]: T[K] extends object ? Draft<T[K]> : T[K];
 };
 
-type OPaths<T> = T extends object
-  ? {
-      [K in keyof T]: K extends string
-        ? T[K] extends Array<infer U>
-          ? `${K}` | `${K}[${number}]` | (U extends object ? `${K}[${number}].${OPaths<U>}` : never)
-          : `${K}` | (T[K] extends object ? `${K}.${OPaths<T[K]>}` : never)
-        : never;
-    }[keyof T]
-  : never;
