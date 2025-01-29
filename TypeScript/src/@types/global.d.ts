88996975910a39747bf5fe041b568e30edfdfbd1
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-01-29 19:51:12 +0530

    Add OTypes and Replacer types for enhanced type manipulation and object transformation

--- TypeScript/src/@types/global.d.ts
+++ TypeScript/src/@types/global.d.ts
@@ -6,3 +6,26 @@ type Draft<T extends object> = {
   -readonly [K in keyof T]: T[K] extends object ? Draft<T[K]> : T[K];
 };
 
+type OTypes<T> = T extends Primitive
+  ? T
+  : T extends Array<infer U>
+    ? U extends object
+      ? OTypes<U>[]
+      : U
+    : T extends object
+      ? OTypes<T[keyof T]>
+      : never;
+
+type Replacer<O, Replace extends Primitive, With extends Primitive> = {
+  [K in keyof O]: O[K] extends Replace
+    ? With
+    : O[K] extends (infer U)[]
+      ? U extends Replace
+        ? With[]
+        : U extends object
+          ? Replacer<U, Replace, With>[]
+          : O[K]
+      : O[K] extends object
+        ? Replacer<O[K], Replace, With>
+        : O[K];
+};
