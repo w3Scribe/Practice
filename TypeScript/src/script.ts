e2b666cc548fcbcf5eedbea28faf4d27ff763444
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-02-04 19:16:09 +0530

    Refactor TRemove type definition for improved functionality; update test case for validation

--- TypeScript/src/script.ts
+++ TypeScript/src/script.ts
@@ -1,8 +1,10 @@
 type FruitTupple = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape'];
 
-type TRemove<T1 extends any[], T2> = T1 extends [...infer Rest, T2]
-  ? Rest
-  : never;
+type TRemove<T extends any[], E extends T[number]> = T extends [infer First, ...infer Rest]
+  ? First extends E
+    ? Rest
+    : [First, ...TRemove<Rest, E>]
+  : T;
 
-type Test = TRemove<FruitTupple, 'banana'>; // i am getting never here find the issue and fix it
-//  after fixing this you have to explain where made mistake and why it is not working
+
+type test = TRemove<FruitTupple, 'elderberry'>;
