e157879415565b1891b13004d9cc387bdc1f91ec
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-19 18:52:45 +0530

    Remove obsolete Ensure module files and update project structure; add index.ts for entry point

--- TypeScript/README.md
+++ TypeScript/README.md
@@ -9,7 +9,7 @@ bun install
 To run:
 
 ```bash
-bun run script.ts
+bun run index.ts
 ```
 
-This project was created using `bun init` in bun v1.1.8. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+This project was created using `bun init` in bun v1.2.5. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- go/app.go
+++ go/app.go
@@ -3,7 +3,59 @@ package main
 import "fmt"
 
 func main() {
-	const name = "Gopher"
-	const nameLen = len(name)
-	fmt.Printf("Hello, %s! Your name contains %d characters.\n", name, nameLen)
+	// Variable
+	// var name string = "Suraj Gadpayle"
+	// fmt.Println(name)
+
+	// Dynamic Variable 
+	// fname := "Suraj"
+	// lname := "Gadpayle"
+	// fmt.Println(fname + " " + lname)
+
+	// Dynamic Multiple Variable inline
+	// fname, laname := "Suraj", "Gadpayle"
+	// fmt.Println(fname + " " + laname)
+
+	// const greeting = "Hello, constant world!"
+	// fmt.Println(greeting)
+
+	// List 
+	// Fruits := []string{"Apple", "Banana", "Orange"}
+	// fmt.Println(Fruits)
+
+	// Map
+	// Person := make(map[string]string)
+	// Person["name"] = "Suraj"
+	// Person["age"] = "25"
+	// Person["city"] = "Pune"
+	// fmt.Println(Person)
+
+	// Loop
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// // Range Loop
+	// Fruits := []string{"Apple", "Banana", "Orange"}
+
+	// for i, value := range Fruits {
+	// 	fmt.Println(i, value)
+	// }
+  
+
+	// Map & loop
+  // Person := make(map[string]string)
+	// Person["name"] = "Suraj"
+	// Person["age"] = "25"
+	// Person["city"] = "Pune"
+
+	// for key, value := range Person {
+	// 	fmt.Println(key, value)
+	// }
+
+	// Function
+	func Hello() {
+		fmt.Println("Hello, World!")
+	}
+
 }
