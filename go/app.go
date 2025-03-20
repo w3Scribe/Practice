9fa00250d5d836a238326b1f9c639faf848ec470
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-20 20:51:49 +0530

    Add input validation and streamline output for WeirdAlgorithm function

--- go/app.go
+++ go/app.go
@@ -1,20 +1,28 @@
 package main
 
-func WeirdAlogorithm(input int) {
-	current := input
-	list := []int{current}
+import "fmt"
 
+// WeirdAlogorithm computes the Collatz sequence for a given number
+func WeirdAlogorithm(input int) {
+	// Validate input
+	if input <= 0 {
+		fmt.Println("Input must be a positive integer")
+		return
+	}
+
+	// Print the first number immediately
+	current := input
+	fmt.Println(current)
+
+	// Calculate and print each number in the sequence as we go
+	// This avoids storing the entire sequence in memory
 	for current != 1 {
 		if current%2 == 0 {
-			current = current / 2
+			current /= 2
 		} else {
 			current = 3*current + 1
 		}
-
-		list = append(list, current)
-	}
-	for _, num := range list {
-		println(num)
+		fmt.Println(current)
 	}
 }
 
