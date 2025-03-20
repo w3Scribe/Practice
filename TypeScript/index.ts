05557aa8f4e161e2f6d7ac9fd103f61f21a014d5
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-20 17:02:02 +0530

    Implement weirdAlgorithm function to demonstrate number transformation sequence

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, TypeScript!");
+function weirdAlogorithm(input: number): void {
+  let current = input;
+  const result = [input]
+  
+  while (current !== 1) {
+    current = current % 2 === 0 ? current / 2 : current * 3 + 1;
+    result.push(current);
+  }
+
+  console.log(result.join(' '));
+}
+
+weirdAlogorithm(3);
