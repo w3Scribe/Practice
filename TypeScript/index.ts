a6164787013e66af6c833ee7ff288b93c99f8d74
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-21 10:28:04 +0530

    Fix formatting output in TypeScript example to include currency representation

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,3 +1,3 @@
 const number = 1_000_000
 const result = Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(number)
-console.log(number, result)
+console.log(number, result)   
