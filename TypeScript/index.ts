fd1dbec21a9c06d7da3458eccde93bb3c56ccd6c
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-21 14:28:23 +0530

    Implement repetition algorithm to find the most repeated character in a string

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,5 +1,23 @@
-const someNumber = 102333333333333.0282
-const decimalResult = someNumber.toPrecision(4)
+function repetitionAlgorithm(input: string) {
+  const charCounts = new Map<string, number>();
+  
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i]!;
+    charCounts.set(char, (charCounts.get(char) || 0) + 1);
+  }
 
+  let maxChar = '';
+  let maxCount = 0;
+  
+  charCounts.forEach((count, char) => {
+    if (count > maxCount) {
+      maxCount = count;
+      maxChar = char;
+    }
+  });
+  
+  console.log(`The most repeated character is "${maxChar}" with a count of ${maxCount}`);
+  return maxChar;
+}
 
-console.log(decimalResult);
+repetitionAlgorithm('hello world!');
