adfa60dc311d4f3b7e51fb4c7372ef3f25ebd17f
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-21 10:27:50 +0530

    Replace Repetition function with currency formatting example in TypeScript

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,21 +1,3 @@
-function Repetition(input: string): void {
-  const Store = new Map<string, number>();
-
-  for (const char of input.split('')) {
-    if (Store.has(char)) {
-      Store.set(char, Store.get(char)! + 1);
-    } else {
-      Store.set(char, 1);
-    }
-  }
-
-  const max = Math.max(...Array.from(Store.values()));
-  const maxChar = Array.from(Store.entries()).find(([_, value]) => value === max)?.[0];
-
-  if (maxChar)
-    console.log(`The most repeated character is "${maxChar}" with ${max} repetitions.`);
-  else
-    console.log("No repeated characters found.");
-}
-
-Repetition('Hello World!');
+const number = 1_000_000
+const result = Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(number)
+console.log(number, result)
