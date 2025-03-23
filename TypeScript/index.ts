74a3e4b231fd50f1097accceae1d3c034f682003
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-23 12:08:42 +0530

    Implement Weird function to generate sequence based on input number

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,26 +1,3 @@
-type MakeArrayNonDecreasing = (nums: number[]) => { moves: number, nums: number[] }
+import { Weird } from './CSES/Introductory/Weird/c1';
 
-const makeArrayNonDecreasing: MakeArrayNonDecreasing = (nums) => {
-  if (!Array.isArray(nums)) {
-    throw new TypeError('Input must be an array', { cause: "InvalidInput" });
-  }
-
-  if (!nums.length) {
-    throw new RangeError('Input array must contain at least one element', { cause: "InvalidInput" });
-  }
-
-  let moves = 0;
-  const result = nums.slice(); 
-
-  for (let i = 1; i < result.length; i++) {
-    if (result[i]! < result[i - 1]!) {
-      const diff = result[i - 1]! - result[i]!;
-      result[i]! += diff;
-      moves += diff;
-    }
-  }
-
-  return { moves, nums: result };
-}
-
-console.log(makeArrayNonDecreasing([3, 2, 5, 1, 7]));
+console.log(Weird(3));
