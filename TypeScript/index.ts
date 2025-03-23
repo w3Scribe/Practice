1d908843f2003270177dd3dcf6b2b18b19662965
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-23 11:41:44 +0530

    Refactor makeArrayNonDecreasing function to enhance input validation and error handling

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,5 +1,33 @@
-// function makeArrayNonDecreasing
-// Return type
-// function input validation and error handling
-// function logic implementation
 
+type MakeArrayNonDecreasing = (nums: number[]) => { moves: number, nums: number[] }
+
+const makeArrayNonDecreasing: MakeArrayNonDecreasing = (nums) => {
+  if (!Array.isArray(nums)) {
+    throw new TypeError('Input must be an array', { cause: "InvalidInput" });
+  }
+
+  if (!nums.length) {
+    throw new RangeError('Input array must contain at least one element', { cause: "InvalidInput" });
+  }
+
+  let Moves = 0;
+  let Result: number[] = [nums[0]!];
+
+  for (let i = 1; i < nums.length; i++) {
+    const prevElement = Result[i - 1]!;
+    const currentElement = nums[i]!
+    const diff = prevElement - currentElement
+
+    if (currentElement < prevElement) {
+      Result.push(currentElement + diff);
+      Moves += diff
+    } else {
+      Result.push(currentElement);
+    }
+  }
+
+  return { moves: Moves, nums: Result }
+}
+
+
+console.log(makeArrayNonDecreasing([3, 2, 5, 1, 7] ))
