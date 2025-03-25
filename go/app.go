6e5e29048386e0acaf01ebe9ebde0a751668cff1
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-03-25 12:58:32 +0530

    Refactor app.go to improve formatting of the Person struct and enhance user output display

--- go/app.go
+++ go/app.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	type Person struct {
 		Name string
-		Age int
+		Age  int
 	}
 
 	User := make(map[string]Person)
@@ -13,5 +13,10 @@ func main() {
 	User["user2"] = Person{"Jane", 25}
 	User["user3"] = Person{"Doe", 40}
 
-	fmt.Print("User: ", User, "\n");
+	for key, value := range User {
+		fmt.Println("Key:", key)
+		fmt.Println("Name:", value.Name)
+		fmt.Println("Age:", value.Age)
+		fmt.Println()
+	}
 }
