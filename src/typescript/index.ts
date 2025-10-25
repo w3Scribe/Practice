480e1518f69fc5df4c8ac9491f0177b75dd3bf12
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-10-25 14:52:02 +0530

    Replace placeholder index.ts with DeepReadOnly type utility and test cases

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,1 +1,54 @@
-console.log("Hello World");
+type IsOject<T extends unknown> =
+  [T] extends [object] ?
+  [T] extends [
+    Function |
+    any[] |
+    Date |
+    RegExp |
+    WeakMap<any, any> |
+    WeakSet<any> |
+    Map<any, any> |
+    Set<any> |
+    Error
+  ] ? false : true
+  : false;
+
+
+type DeepReadOnly<T> = IsOject<T> extends true
+  ? { readonly [K in keyof T]: DeepReadOnly<T[K]> }
+  : T;
+
+// Test cases
+type A = DeepReadOnly<{
+  a: {
+    b: {
+      c: string;
+    };
+    d: number[];
+    e: () => void;
+    f: Date;
+    g: {
+      h: {
+        i: boolean;
+      };
+    };
+  };
+}>;
+
+// Expected:
+// type A = {
+//   readonly a: {
+//     readonly b: {
+//       readonly c: string;
+//     };
+//     readonly d: readonly number[];
+//     readonly e: () => void;
+//     readonly f: Date;
+//     readonly g: {
+//       readonly h: {
+//         readonly i: boolean;
+//       };
+//     };
+//   };
+// }
+
