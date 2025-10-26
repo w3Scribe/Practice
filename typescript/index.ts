c5091d5bfd4fa526159b791a62059e9a686d603f
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-10-26 19:27:47 +0530

    chore(ts): remove TupleToUnion utilities from typescript/index.ts

--- typescript/index.ts
+++
@@ -1,5 +0,0 @@
-// Tupple to union
-
-type TuppleToUnion<T extends any[]> = T[number]
-type Tupple2Union<T extends any[]> = T extends Array<infer AE> ? AE : never
-type Tupple3Union<T extends any[]> = T extends (infer AE)[] ? AE : never
