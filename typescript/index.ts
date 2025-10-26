4420a6afa1c19885598d4e80dc8c9ce3a6324481
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2025-10-26 18:53:55 +0530

    Checkpoint from VS Code for coding agent session

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,18 @@
-type IsEq<T1, T2> = 
-   (<U>()=> U extends T1 ? 1 : 2) extends
-  (<U>() => U extends T2 ? 1 : 2) ? true : false;
-   
+// Tupple to union
+
+type TuppleToUnion<T extends any[]> = T[number]
+
+type Tupple2Union<T extends any[]> = T extends Array<infer AE> ? AE : never
+
+// Do you have a better way to solve this problem?
+
+// TODO: make a function that converts a tuple to a union
+
+
+
+// test 
+type tupple = ["Hello", "World"]
+
+type test1 = TuppleToUnion<tupple>
+
+type test2 = Tupple2Union<tupple>
